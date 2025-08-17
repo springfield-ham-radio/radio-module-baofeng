6a382817ee00d3331a2d8813a671a38f1a5231a1
Bryan Hunt <bhunt@mac.com>
2025-08-17 10:04:46 -0500

    chore: better oxlint config

--- src/shared/codecs/baofeng-dcs-tones.ts
+++ src/shared/codecs/baofeng-dcs-tones.ts
@@ -1,11 +1,12 @@
 import { DCS } from '@springfield/ham-radio-api';
 
-export const dcsValues: DCS[] = Object.values(DCS).filter(value => 'number' === typeof value) as DCS[];
+export const dcsValues: DCS[] = Object.values(DCS).filter(value => typeof value === 'number') as DCS[];
 
 export const dcsByValue = new Map<number, DCS>(); // index -> DCS value
 export const valuesByDcs = new Map<DCS, number>(); // DCS value -> index
 
-dcsValues.forEach((value, index) => {
+for (let index = 0; index < dcsValues.length; index += 1) {
+  const value = dcsValues[index];
   dcsByValue.set(index, value);
   valuesByDcs.set(value, index);
-});
+}
